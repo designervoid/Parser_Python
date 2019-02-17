e4aae80b922991b68c9cded8f57c3588b411bf75
designervoid <43455658+designervoid@users.noreply.github.com>
2019-02-17 21:44:39 +0300

    Update and rename pars.py to json.dictionary.py
    Changed path to file.

--- json.dictionary.py
+++ json.dictionary.py
@@ -9,7 +9,7 @@ def add_gender_to_message(message, authors):
 
 def get_authors_with_gender():
     authors_genders = {}
-    file = open('/home/sax/task_python/public.jsonlines', 'r')
+    file = open('path to file => public.jsonlines', 'r')
     for line in file:
         author = json.loads(line)
         author_id = author["author"]
@@ -20,8 +20,8 @@ def get_authors_with_gender():
 
 def main():
     authors = get_authors_with_gender()
-    with open('/home/sax/task_python/messages.jsonlines', 'r') as file, \
-            open('/home/sax/task_python/new_file.jsonlines', 'a+') as new_file:
+    with open('path to file => messages.jsonlines', 'r') as file, \
+            open('path to file => new_file.jsonlines', 'a+') as new_file:
         for line in file:
             message = json.loads(line)
             add_gender_to_message(message, authors)
